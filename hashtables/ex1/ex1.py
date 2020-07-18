8c20b906f7dcd80dd07301a4773e506a543120eb
Dominick Bruno <dbruno@unseen.is>
2020-07-17 21:39:02 -0400

    Complete Ex1 4/4

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,42 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    # Initialize Hash to hold key:value (weight:index)
+    
+    weightHash = {}
+
+    # Loop through list and compare weights to Limit
+
+    for i in range(length):
+
+        currVal = limit - weights[i]
+
+        # Check table for currVal
+
+        if currVal in weightHash:
+
+            # Conform to zeroth and first requirement
+
+            first = weightHash[currVal]
+            solution = (i, first)
+            return solution
+
+        # If currVal not present, proceed
+
+        else:
+                weightHash[weights[i]] = i
+
+    # exit if limit is never matched
+
+    return None
+
+
+
+
+
+    
+
+
+
+
 
     return None
